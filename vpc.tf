035a866c06a7ddf7546b6ac94c3cb4c497fffa70
harishdevops3 <harishdevops2003@gmail.com>
2024-08-27 07:38:06 +0530

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-module "harish_vpc" {
-    source = "./modules"
-    vpc_cidr = "192.168.0.0/16"
-    private_subnet_cidr = [ "192.168.0.0/18", "192.168.64.0/18" ] 
-    public_subnet_cidr = [ "192.168.128.0/18", "192.168.192.0/18" ]
-}
-
-
